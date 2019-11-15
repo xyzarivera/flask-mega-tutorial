a2af541eefee28602101c5fc6b31e1c037f8d752
Xyza Rivera <xyzarivera@gmail.com>
2019-11-15 17:47:13 +0800

    adds from form data validation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
+from flask import render_template, flash, redirect
+
 
 @app.route('/')
 @app.route('/index')
@@ -21,4 +23,8 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
