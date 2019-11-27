a19b6f8af590864f9d280a8bf3930b40a9f1b227
Xyza Rivera <xyzarivera@gmail.com>
2019-11-27 18:32:31 +0800

    fixed submit variable

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sig
+    submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
