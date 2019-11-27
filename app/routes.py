07e132afdad1c01da7be58b089ae4a4d64e49fd3
Xyza Rivera <xyzarivera@gmail.com>
2019-11-27 18:14:27 +0800

    adds login route

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,8 @@ from flask import render_template
 from app import app
 from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
-
+from flask_login import current_user, login_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -26,5 +27,19 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
